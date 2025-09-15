f133211283a6152e4b20e2a6fe0029dcbdcdf0dd
d0miep <dominiquerangelperedo@gmail.com>
2025-09-15 13:33:45 -0600

    feat: prompt three comma-separated numbers and display them

--- solucion.js
+++ solucion.js
@@ -1,1 +1,5 @@
+let cadena = prompt("Escriba tres números con el formato número1,número2,número3:");
+const numeros = texto.split(",");
+alert(numeros)
+//alert(`Números ordenados de mayor a menor: ${mayorMenor} \n Números ordenados de menor a mayor: ${menorMayor}`);
 
