7a12c0b34bd993ad6dea0bd632588ff6be5a7f9b
domieprangel <dominiquerangelperedo@gmail.com>
2025-09-15 14:09:08 -0600

    refactor: update program logic for number input and processing

--- solucion.js
+++ solucion.js
@@ -1,5 +1,16 @@
 let cadena = prompt("Escriba tres números con el formato número1,número2,número3:");
-const numeros = texto.split(",");
-alert(numeros)
-//alert(`Números ordenados de mayor a menor: ${mayorMenor} \n Números ordenados de menor a mayor: ${menorMayor}`);
+const numeros = cadena.split(",");
+const maximo = Math.max(...numeros);
+const minimo = Math.min(...numeros);
+let centro;
+if (maximo === minimo){
+    centro = maximo;
+}else{
+    for (const num of numeros) {
+        (num != maximo && num != minimo) ? centro = num: (num === maximo) ? centro = maximo: centro = minimo;
+    }
+}
+const mayorMenor = [maximo, centro, minimo];
+const menorMayor = [minimo, centro, maximo];
+alert(`Números ordenados de mayor a menor: ${mayorMenor} \n Números ordenados de menor a mayor: ${menorMayor} \n Maximo: ${maximo}`);
 
